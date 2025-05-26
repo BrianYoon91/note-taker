13d4f9c8c83d462d3770c205cb3c50252b2c0f46
Brian Yoon <yoon.brian91@gmail.com>
2025-05-26 16:30:59 -0400

    note taker first commit

---
+++ src/NewNotes.tsx
@@ -0,0 +1,15 @@
+import { NoteForm } from "./NoteForm";
+import { type NoteData } from "./App";
+
+type NewNoteProps = {
+	onSubmit: (data: NoteData) => void;
+};
+
+export function NewNote({ onSubmit }: NewNoteProps) {
+	return (
+		<>
+			<h1 className="mb-4">New Note</h1>
+			<NoteForm onSubmit={onSubmit} />
+		</>
+	);
+}
